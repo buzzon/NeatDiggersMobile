bdc72f853e77754baf1c5dfc33c4700ec7fab042
buzzon <buzzondev@gmail.com>
2021-01-28 15:46:02 +0500

    MobileCameraControl

---
+++ Assets/Scripts/MobileCameraControl.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileCameraControl : MonoBehaviour
+{
+    Vector3 FirstPoint;
+    Vector3 SecondPoint;
+    float xAngle;
+    float yAngle;
+    float xAngleTemp;
+    float yAngleTemp;
+
+    float TouchZoomSpeed = 0.02f;
+    float ZoomMinBound = 10.0f;
+    float ZoomMaxBound = 100.0f;
+    Camera cam;
+    bool IsZooming = false;
+
+    void Start()
+    {
+        xAngle = 0;
+        yAngle = 0;
+        transform.rotation = Quaternion.Euler(yAngle, xAngle, 0);
+        cam = GetComponentInChildren<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount == 0)
+        {
+            IsZooming = false;
+        }
+        if (Input.touchCount == 1 && !IsZooming)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                FirstPoint = Input.GetTouch(0).position;
+                xAngleTemp = xAngle;
+                yAngleTemp = yAngle;
+            }
+            if (Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                SecondPoint = Input.GetTouch(0).position;
+                xAngle = xAngleTemp + (SecondPoint.x - FirstPoint.x) * 180 / Screen.width;
+                yAngle = yAngleTemp - (SecondPoint.y - FirstPoint.y) * 90 / Screen.height;
+                this.transform.rotation = Quaternion.Euler(yAngle, xAngle, 0.0f);
+            }
+        }
+        if (Input.touchCount == 2)
+        {
+            IsZooming = true;
+            Touch tZero = Input.GetTouch(0);
+            Touch tOne = Input.GetTouch(1);
+
+            Vector2 tZeroPrevious = tZero.position - tZero.deltaPosition;
+            Vector2 tOnePrevious = tOne.position - tOne.deltaPosition;
+
+            float oldTouchDistance = Vector2.Distance(tZeroPrevious, tOnePrevious);
+            float currentTouchDistance = Vector2.Distance(tZero.position, tOne.position);
+
+            float deltaDistance = oldTouchDistance - currentTouchDistance;
+            Zoom(deltaDistance, TouchZoomSpeed);
+        }
+
+    }
+
+    void Zoom(float deltaMagnitudeDiff, float speed)
+    {
+
+        cam.fieldOfView += deltaMagnitudeDiff * speed;
+        cam.fieldOfView = Mathf.Clamp(cam.fieldOfView, ZoomMinBound, ZoomMaxBound);
+    }
+}
